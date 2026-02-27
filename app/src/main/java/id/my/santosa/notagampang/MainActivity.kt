7f3c569e8c8635cac95be6bb24aa06eba9b5d7b0
Prima Santosa <prima@santosa.my.id>
2026-02-27 15:05:42 +0700

    style: configure ktlint with 2-space indentation

--- app/src/main/java/id/my/santosa/notagampang/MainActivity.kt
+++ app/src/main/java/id/my/santosa/notagampang/MainActivity.kt
@@ -12,26 +12,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme {
-                Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                ) { Greeting("Notagampang") }
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      MaterialTheme {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background,
+        ) { Greeting("Notagampang") }
+      }
     }
+  }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
+fun Greeting(
+  name: String,
+  modifier: Modifier = Modifier,
+) {
+  Text(text = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MaterialTheme { Greeting("Notagampang") }
+  MaterialTheme { Greeting("Notagampang") }
 }
